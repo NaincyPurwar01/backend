84c821b84411ff49cf800414d0794c45b8351966
ABC <naincypurwar2@gmail.com>
2023-11-26 16:04:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/' , (req , res) =>{
+    res.send('Hello World!')
+})
+
+
+app.get('/twitter' , (req , res) => {
+    res.send('naincydotcom')
+})
+
+app.get('/login' , (req , res) => {
+    res.send('<h1> please login to my app</h1>')
+})
+
+app.get('/youtube' , (req , res) => {
+    res.send('<h2>Cahi aur code</h2>')
+})
+app.listen(port.env.PORT , () => {
+    console.log(`Example app listening on port ${port}`);
+})
